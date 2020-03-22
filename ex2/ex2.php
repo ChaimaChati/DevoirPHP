b30b2cb5c18572180f7c2284ae2c6bbd59b22b0d
Chaima Chati <45006780+ChaimaChati@users.noreply.github.com>
2020-03-22 14:16:51 +0100

    Add files via upload

---
+++ ex2/ex2.php
@@ -0,0 +1,14 @@
+<?php
+
+	$file = file("file.txt") or die("Impossible d'ouvrir le fichier!");
+    $total = count($file);
+
+    for($i = 0; $i < $total; $i++) {
+   
+    echo nl2br($file[$i]);
+    }
+
+ 
+
+
+?>
---
+++ ex3/ex3.php
@@ -0,0 +1,41 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ex3</title>
+</head>
+<body>
+
+    <h1>Centrale d'achats</h1>
+    <h2>Commande Clients</h2>
+        <table border=1 style="width: 800px;">
+    <tr bgcolor='lightsteelblue'>
+        <th>Numero de commande</th>
+        <th>Numero Client</th>
+        <th>Date de commande</th>
+        <th>Désignation article/th>
+        <th>Quantité</th>
+        <th>prix unitaire</th>
+        <th>Date de livraison</th>
+        <th>Adresse client</th>
+    </tr>
+<?php
+ $file = fopen("file.txt", "r") or die("Impossible d'ouvrir le fichier!");
+
+    while (!feof($file)) {
+        $ligne = fgets($file);
+        echo"<tr>";
+        $arr=explode("|",$ligne);
+
+        foreach($arr as $cellule){
+        echo "<td>".$cellule."</td>";
+    }
+    echo"</tr>";
+    }
+
+    fclose($file);
+
+?>
+</table>
+</body>
+</html>
